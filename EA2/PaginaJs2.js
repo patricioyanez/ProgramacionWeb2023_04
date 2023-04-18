b46f2802647280bb47134da546b71215d4516cdf
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-18 18:33:39 -0400

    update

--- EA2/PaginaJs2.js
+++ EA2/PaginaJs2.js
@@ -1,4 +1,4 @@
-function miFuncion(){
+function sumar(){
     var n1 = document.getElementById("num1").value;
     var n2 = document.getElementById("num2").value;
     let total = parseInt(n1) + parseInt(n2);
@@ -7,3 +7,31 @@ function miFuncion(){
 
 // Ejercicios: Agregar botones y funciones para realizar:
 // resta, multiplicación y división (evitar dividir por cero).
+
+function restar(){
+    var n1 = document.getElementById("num1").value;
+    var n2 = document.getElementById("num2").value;
+    let total = parseInt(n1) - parseInt(n2);
+    document.getElementById("res").value = total;
+}
+function dividir(){
+    var n1 = document.getElementById("num1").value;
+    var n2 = document.getElementById("num2").value;
+    n1 = parseInt(n1)
+    n2 = parseInt(n2)
+    
+    if(n2 == 0)
+    {
+        alert("No se puede dividir por cero");
+        return false;
+    }
+
+    let total = n1/n2;
+    document.getElementById("res").value = total;
+}
+function multiplicar(){
+    var n1 = document.getElementById("num1").value;
+    var n2 = document.getElementById("num2").value;
+    let total = parseInt(n1) * parseInt(n2);
+    document.getElementById("res").value = total;
+}
