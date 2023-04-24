e086cf4f7089a8761d2f2b4b1872964c5832ab89
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-24 18:07:26 -0400

    update

--- EA2/paginajQueryValidar.js
+++ EA2/paginajQueryValidar.js
@@ -1,5 +1,6 @@
 $(function()
 {
+    let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/
     $('.btnAceptar').click(function()
     {
         if(!$('.txtRut').val())
@@ -17,5 +18,45 @@ $(function()
             alert('Falta el nombre');
             $('.txtNombre').focus();
         }
+        else if(!emailRegex.test($('.txtEmail').val()))
+        { //verifica que el formato del correo este correcto
+            alert('El formato del correo no es correcto');
+            $('.txtEmail').focus();
+        }
+    });
+    $('.btnLimpiar').click(function()
+    {
+        $('.txtRut, .txtDv, .txtNombre').val('');
+        $('.txtRut').focus();
+    });
+
+    // definir patrones de caracteres a permitir
+    let numeros = '123457890';
+    let letras = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNMÁÉÍÓÚáéíóú ';
+
+    $('.txtRut').keypress(function(e)
+    {
+        // obtiene el codigo del caracter presionado y es convertido a 
+        // el mismo caracter
+        let caracter = String.fromCharCode(e.which); 
+        if(numeros.indexOf(caracter)<0) // verifica si el caracter esta en el patrón
+            return false; // evita que se escriba el caracter presionado
+    });
+    $('.txtDv').keypress(function(e)
+    {
+        // obtiene el codigo del caracter presionado y es convertido a 
+        // el mismo caracter
+        let dv = numeros + 'kK';
+        let caracter = String.fromCharCode(e.which); 
+        if(dv.indexOf(caracter)<0) // verifica si el caracter esta en el patrón
+            return false; // evita que se escriba el caracter presionado
+    });
+    $('.txtNombre').keypress(function(e)
+    {
+        // obtiene el codigo del caracter presionado y es convertido a 
+        // el mismo caracter
+        let caracter = String.fromCharCode(e.which); 
+        if(letras.indexOf(caracter)<0) // verifica si el caracter esta en el patrón
+            return false; // evita que se escriba el caracter presionado
     });
 })
