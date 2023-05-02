ee4f5c936846744384cd9d2bc90b1591627ea9da
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-02 18:16:06 -0400

    update

--- EA2/paginajQueryValidar.js
+++ EA2/paginajQueryValidar.js
@@ -67,25 +67,20 @@ $(function()
 
     function esValidoElRut(Rut,Digito)
     {
-        Digito = Digito.toUpperCase();
-		var longitud        = Rut.length;
-		var factor          = 2;
-		var sumaProducto    = 0;
-		var con             = 0;
-		var caracter     	= 0;
+		let factor          = 2;
+		let sumaProducto    = 0;
+		let con             = 0;
+		let caracter     	= 0;
  
-		for( con=longitud-1; con>=0; con--)
+		for( con=Rut.length-1; con>=0; con--)
 		{
 			caracter = Rut.charAt(con);
 			sumaProducto += (factor * caracter);
 			if (++factor > 7)
 				factor=2;		
 		}
- 
-        var digitoAuxiliar	= 11-(sumaProducto % 11);   
-        var caracteres		= "-123456789K0";
-        var digitoCaracter= caracteres.charAt(digitoAuxiliar);
-        return digitoCaracter == Digito ? 1 : 0;            
+        var digitoCaracter= "-123456789K0".charAt(11-(sumaProducto % 11));
+        return digitoCaracter == Digito.toUpperCase();            
     } 
 
 })
