4e049688e9c2470a943edd000b173b7bd31563e8
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-23 18:38:24 -0400

    update

--- EA3/proyecto004/instituto/crud/admin.py
+++ EA3/proyecto004/instituto/crud/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Genero
+from .models import Genero, Categoria, Marca, Cliente
 # Register your models here.
 
 admin.site.register(Genero)
+admin.site.register(Categoria)
+admin.site.register(Marca)
+
+# configurar que columnas se van a mostrar
+# configurar filtros
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['rut', 'nombre', 'apellido', 'genero']
+    list_filter  = ['genero', 'apellido']
+
+admin.site.register(Cliente, ClienteAdmin)
--- EA3/proyecto004/instituto/crud/models.py
+++ EA3/proyecto004/instituto/crud/models.py
@@ -8,6 +8,49 @@ class Genero(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Cliente(models.Model):
+    rut = models.IntegerField(unique=True, null=False, blank=False)
+    dv = models.CharField(max_length=1, null=False, blank=False, default="")
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    direccion = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    telefono = models.CharField(max_length=20, null=False, blank=False)
+    genero = models.ForeignKey(Genero, db_column='idGenero', 
+                                on_delete=models.CASCADE, null=False, blank=False)
+    fechaNacimiento = models.DateField(null=False, blank=False)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
+
+
+
+
+
+class Marca(models.Model):
+    id_marca= models.AutoField(db_column='idMarca', primary_key=True)
+    nombre = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.nombre
     
+class Categoria(models.Model):
+    id_categoria= models.AutoField(db_column='idCategoria', primary_key=True)
+    nombre = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.nombre
+
 ## Marca
 ## Categoria
+
+# PASOS:
+# 1.- para preparar la migración:
+# py manage.py makemigrations crud
+# 2.- hacer migracion
+# py manage.py migrate crud
+
+### otras clases que representan los modelos
